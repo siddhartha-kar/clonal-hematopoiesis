632920f35d5274187a21eeec8eb20c105ab09ee2
Siddhartha Kar <62653927+siddhartha-kar@users.noreply.github.com>
2022-02-17 23:27:02 +0000

    Add files via upload

---
+++ mendelian_randomization/large_as_out_mr_input.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+library(vroom)
+
+d <- vroom(file = "chiplarge_bolt_0.3info_0.00005MAF_filtered.out", delim = "\t")
+
+d <- d %>% filter(INFO > 0.3)
+
+d <- d %>% filter(A1FREQ > 0.01)
+
+d <- d %>% filter(A1FREQ < 0.99)
+
+d <- d %>% mutate(BETA1 = BETA/((4049/(4049+173918))*(1-(4049/(4049+173918)))))
+
+d <- d %>% mutate(SE1 = SE/((4049/(4049+173918))*(1-(4049/(4049+173918)))))
+
+write.table(d, file = "large_mr_input", sep = "\t", row.names = F, quote = F)
---
+++ mendelian_randomization/small_as_out_mr_input.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+
+library(vroom)
+
+d <- vroom(file = "chipsmall_bolt_0.3info_0.00005MAF_filtered.out", delim = "\t")
+
+d <- d %>% filter(INFO > 0.3)
+
+d <- d %>% filter(A1FREQ > 0.01)
+
+d <- d %>% filter(A1FREQ < 0.99)
+
+d <- d %>% mutate(BETA1 = BETA/((6154/(6154+173918))*(1-(6154/(6154+173918)))))
+
+d <- d %>% mutate(SE1 = SE/((6154/(6154+173918))*(1-(6154/(6154+173918)))))
+
+write.table(d, file = "small_mr_input.txt", sep = "\t", row.names = F, quote = F)
